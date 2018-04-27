930aef50baf12f15da4146633c87a4c01f6f1f86
Mark Brooks <Markabrooks@gmail.com>
2018-04-27 16:39:56 -0600

    wired reduxForm and created initial title input field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderTitleField(field){  //field contains event handlers which makes sure Field deals with this text input
+    return(
+      <div>
+        <input
+          type="text"
+          {...field.input}  //contains pregenerated event handlers ... passes all these as props
+        />
+      </div>
+
+    );
+  }
+
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field        //used to describe unique input in form.  Communicates with reduxForm
+          name="title"
+          component={this.renderTitleField}  //tells Field what to look like when displayed
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({   //wraps reduxForm around PostsNew
+  form: 'PostsNewForm'       //namespace for this specific form component
+})(PostsNew);
