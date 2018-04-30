c3792f86904055302a7f6fad5f3de62db510a5d9
Mark Brooks <Markabrooks@gmail.com>
2018-04-30 17:22:38 -0600

    wired PostsShow component

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action){
   switch (action.type){
+    case FETCH_POST:
+      
+      return { ...state, [action.payload.data.id]: action.payload.data };  //this line is same as commented lines above
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id'); //.mapKeys takes an array and the value you want to create as the key
     default:
