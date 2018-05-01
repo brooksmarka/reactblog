858ce5652a0c30ab39058000780a183955ced414
Mark Brooks <Markabrooks@gmail.com>
2018-05-01 11:49:41 -0600

    added links for posts and link to index from post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,7 +4,10 @@ import { FETCH_POSTS, FETCH_POST } from '../actions';
 export default function(state = {}, action){
   switch (action.type){
     case FETCH_POST:
-      
+      //const post = action.payload.data;
+      //const newState = { ...state };
+      //newState[post.id] = post;
+      //return newState;
       return { ...state, [action.payload.data.id]: action.payload.data };  //this line is same as commented lines above
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id'); //.mapKeys takes an array and the value you want to create as the key
